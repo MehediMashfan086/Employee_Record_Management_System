e19cf0a572b1598aa4095af83190ea44a7e45271
MehediMashfan086 <mehedihasan62086@gmail.com>
2021-10-18 09:56:19 +0600

    Saving to GitHub

--- employee/views.py
+++ employee/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 
@@ -6,4 +7,17 @@ def index(request):
     return render(request, 'index.html')
 
 def registration(request):
+    error = ""
+    if request.method == "POST":
+        fn = request.POST['firstname']
+        ln = request.POST['lastname']
+        ec = request.POST['employeecode']
+        em = request.POST['emailid']
+        pwd = request.POST['pwd']
+        try:
+            User.object.create_user(first_name = fn, last_name = ln, username = em, password = pwd)
+            EmployeeInfo.objects.create(employeecode = ec)
+        except:
+            error = "Yes"
+    
     return render(request, 'registration.html')
