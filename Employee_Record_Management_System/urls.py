f1d527458ad6deb4dfa40de8e285ab5659529fcc
MehediMashfan086 <mehedihasan62086@gmail.com>
2021-10-19 12:51:51 +0600

    Saving to GitHub

--- Employee_Record_Management_System/urls.py
+++ Employee_Record_Management_System/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('', index, name='index'),
     path('registration', registration, name='registration'),
     path('emp_login', emp_login, name='emp_login'),
+    path('emp_home', emp_home, name='emp_home'),
 ]
