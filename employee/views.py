b721bf01647206226b1ba8e3709c13fbba36c71b
MehediMashfan086 <mehedihasan62086@gmail.com>
2021-10-19 12:52:15 +0600

    Saving to GitHub

--- employee/views.py
+++ employee/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import *
+from django.contrib.auth import login, logout, authenticate
 
 # Create your views here.
 
@@ -12,7 +13,7 @@ def registration(request):
         fn = request.POST['firstname']
         ln = request.POST['lastname']
         ec = request.POST['employeecode']
-        em = request.POST['emailid']
+        em = request.POST['email']
         pwd = request.POST['pwd']
         try:
             user = User.objects.create_user(first_name = fn, last_name = ln, username = em, password = pwd)
@@ -23,4 +24,17 @@ def registration(request):
     return render(request, 'registration.html', locals())
 
 def emp_login(request):
-    return render(request, 'emp_login.html')
+    error = ""
+    if request.method == "POST":
+        u = request.POST.get('emailid')
+        p = request.POST.get('passwd')
+        user = authenticate(username = u, password = p)
+        if user:
+            login(request, user)
+            error = "no"
+        else:
+            error = "yes"
+    return render(request, 'emp_login.html', locals())
+
+def emp_home(request):
+    return render(request, 'emp_home.html')
